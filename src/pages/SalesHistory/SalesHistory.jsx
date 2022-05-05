dfc595d00c434d08c22518bced8f0897c7ed9950
Bestnogm1 <koffiarielhessou@gmail.com>
2022-05-05 17:31:28 -0400

    done

--- src/pages/SalesHistory/SalesHistory.jsx
+++ src/pages/SalesHistory/SalesHistory.jsx
@@ -1,28 +1,20 @@
 import React from 'react';
 import styles from './SalesHistory.module.css'
-import Card from 'react-bootstrap'
+import {Card, ListGroup} from 'react-bootstrap'
 function SalesHistory({jobData}) {
   console.log('here', jobData.jobDoc);
   return (
     <>
-    
       {jobData.jobDoc?.map( (j, index) => (
-          <div>
-            {/* <h1>{j.customer.name}</h1>
-            <h1>{j.customer.History}</h1>
-            <h1>{j.customer.phoneNumber}</h1>
-            <h1>{j.customer.adress}</h1>
-            <h1>{j.product}</h1> */}
-           <div className="card" style={{width: "18rem"}}>
-              <div class="card-header">
-              </div>
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item">{j.customer.History}</li>
-                <li class="list-group-item">{j.customer.phoneNumber}</li>
-                <li class="list-group-item">{j.customer.adress}</li>
-                <li class="list-group-item">{j.product}</li>
-              </ul>
-            </div>
+          <div className={styles.mains}>
+            <Card style={{ width: '18rem' }}>
+            <Card.Header>{j.customer.name}</Card.Header>
+            <ListGroup variant="flush">
+              <ListGroup.Item>Product: {j.product}</ListGroup.Item>
+              <ListGroup.Item> Date: {j.date} </ListGroup.Item>
+              <ListGroup.Item> Date:{j.customer.deposit} </ListGroup.Item>
+            </ListGroup>
+          </Card>
           </div>
           
       ))}
